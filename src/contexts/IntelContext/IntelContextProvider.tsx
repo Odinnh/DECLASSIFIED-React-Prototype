4326bb032200c0e0fbb834ccde630c8a82dd02f4
Ben A <ben@sol3.me>
2022-05-23 01:09:07 +0100

    WIP - Try and call SetMap as a react hook

--- src/contexts/IntelContext/IntelContextProvider.tsx
+++ src/contexts/IntelContext/IntelContextProvider.tsx
@@ -1,22 +1,29 @@
+import L from "leaflet";
 import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { useMapEvents } from "react-leaflet";
+import { SetMap } from "../../helpers/mapUtils";
 import { IntelContextProps } from "./types";
 
 export const IntelContext = createContext<IntelContextProps>({
-    currentMap: "",
-    setCurrentMap: () => { }
+    currentMap: "zoo",
+    setCurrentMap: () => { },
 });
 
 export const IntelContextProvider = ({ children }) => {
-    const [currentMap, setCurrentMapState] = useState("Hello World");
+    const [currentMap, setCurrentMapState] = useState("zoo");
+    const [mapInstance, setMapInstance] = useState(null);
+    const map = useMapEvents({});
 
-    const setCurrentMap = (mapString) => {
-        setCurrentMapState(mapString);
+    const setCurrentMap = (mapId) => {
+        setCurrentMapState(mapId);
+        // set layers in control
+        // set default layer for selected map in map instance
+        SetMap(IntelContext, mapId, map)
     }
-
 
     const context = {
         currentMap,
-        setCurrentMap
+        setCurrentMap,
     }
     return <IntelContext.Provider value={context}>{children}</IntelContext.Provider>;
 }
