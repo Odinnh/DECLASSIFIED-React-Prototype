d2a8e78406f123475cea2a95f245fa27e0b78dc1
Ben A <ben@sol3.me>
2022-05-23 01:07:26 +0100

    Move everything inside map container for the context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import MapProvider from './components/Map';
 import { IntelContextProvider } from './contexts/IntelContext/IntelContextProvider';
 import { BaseLayout } from './pages/layouts/BaseLayout';
-import { MapContainer } from 'react-leaflet';
 
 function App() {
-  
+  document.body.classList.add('dark');
+
   return (
     <BaseLayout>
-      <IntelContextProvider>
-        <MapContainer >
-          <Header />
-        </MapContainer>
-      </IntelContextProvider>
+      <MapProvider />
     </BaseLayout>
   );
 }
---
+++ src/components/Map/index.tsx
@@ -0,0 +1,50 @@
+import L from 'leaflet'
+import React, { useContext } from 'react'
+import { MapContainer, Marker, Popup } from 'react-leaflet'
+import { IntelContext, IntelContextProvider } from '../../contexts/IntelContext/IntelContextProvider'
+import { MapLayers } from '../../helpers/mapLayers'
+import { DefaultIcon } from '../DefaultIcon'
+import Header from '../Header'
+import { IntelMenu } from '../IntelMenu'
+import { MapControls } from '../MapControls'
+import { MapMarkers } from '../MapMarkers'
+
+const MapProvider = () => {
+    const { currentMap } = useContext(IntelContext);
+    console.log("currentMap", currentMap)
+
+    return (
+        <MapContainer id={"worldMap"}
+            center={[256, 256]}
+            zoom={0.8}
+            scrollWheelZoom={true}
+            crs={L.CRS.Simple}
+            maxBounds={[
+                [-256, -256],
+                [768, 768]
+            ]}
+            maxZoom={5}
+            minZoom={0.1}
+            layers={[
+                MapLayers[currentMap].Layer
+            ]}
+            tap={true}
+            tapTolerance={30}
+            /* noWrap={true} */
+            doubleClickZoom={false}
+            zoomDelta={0.5}
+            wheelPxPerZoomLevel={80}
+            zoomSnap={0}
+            maxBoundsViscosity={1.0}
+        >
+            <IntelContextProvider>
+                <Header />
+                <MapMarkers />
+                <MapControls />
+                <IntelMenu />
+            </IntelContextProvider>
+        </MapContainer>
+    )
+}
+
+export default MapProvider;
