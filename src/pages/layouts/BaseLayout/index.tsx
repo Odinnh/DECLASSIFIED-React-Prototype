a41409801187c1b42f13459ad284d9d897488b20
Ben A <ben@sol3.me>
2022-05-23 01:02:24 +0100

    Apply styles

--- src/pages/layouts/BaseLayout/index.tsx
+++ src/pages/layouts/BaseLayout/index.tsx
@@ -1,3 +1,12 @@
+import '../../../styles/worldmap.css';
+import '../../../styles/global.css';
+import '../../../styles/leaflet-override.css';
+import '../../../styles/style.css';
+import '../../../styles/buttons.css';
+import '../../../styles/normalize.css';
+import '../../../styles/challenge/cards-alt.css';
+import '../../../styles/challenge/cards.css';
+import '../../../styles/challenge/mobilefirst.css';
 
 export interface BaseLayoutProps {
     children: React.ReactNode
@@ -5,11 +14,12 @@ export interface BaseLayoutProps {
 
 export const BaseLayout = ({ children }: BaseLayoutProps) => {
     return (
-    <>
-        <header>Test Header</header>
-        <body>
+        <>
+            {/* <header>Test Header</header> */}
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+                integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+                crossOrigin="" />
             {children}
-        </body>
-    </>
+        </>
     );
 }
