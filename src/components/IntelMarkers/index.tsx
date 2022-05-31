745d3c4c6c0c04c1261695d9b28c563903aa6f99
Ben A <ben@sol3.me>
2022-05-31 22:33:23 +0100

    Move map markers to own component

--- src/components/IntelMarkers/index.tsx
+++ src/components/IntelMarkers/index.tsx
@@ -1,18 +1,57 @@
+import { LatLngBoundsExpression, LatLngExpression } from 'leaflet'
 import React, { useContext } from 'react'
-import { LayersControl } from 'react-leaflet'
+import { LayerGroup, LayersControl } from 'react-leaflet'
 import { IntelContext } from '../../contexts/IntelContext/IntelContextProvider'
-import { IntelMapMarker } from '../MapMarker'
 
 export const IntelMarkers = () => {
+    const center: LatLngExpression = [0, 0]
+    const rectangle: LatLngBoundsExpression = [
+        [0, 0],
+        [0, 0],
+    ]
     const { intelArtifactMarkers, intelAudioMarkers } = useContext(IntelContext)
     return (
         <>
             <LayersControl.Overlay name="Intel - Audio Logs">
-                {intelAudioMarkers}
+                <LayerGroup>
+                    {intelAudioMarkers}
+                </LayerGroup>
             </LayersControl.Overlay>
             <LayersControl.Overlay name="Intel - Artifacts">
-                {intelArtifactMarkers}
+                <LayerGroup>
+                    {intelArtifactMarkers}
+                </LayerGroup>
             </LayersControl.Overlay>
+
+            {/* <LayersControl.Overlay checked name="Misc Markers">
+                <LayerGroup>
+                    <Circle
+                        center={center}
+                        pathOptions={{ fillColor: 'blue' }}
+                        radius={200}
+                    />
+                    <Circle
+                        center={center}
+                        pathOptions={{ fillColor: 'red' }}
+                        radius={100}
+                        stroke={false}
+                    />
+                    <LayerGroup>
+                        <Circle
+                            center={[0, 0]}
+                            pathOptions={{ color: 'green', fillColor: 'green' }}
+                            radius={100}
+                        />
+                    </LayerGroup>
+                </LayerGroup>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Easter Eggs">
+                <FeatureGroup pathOptions={{ color: 'purple' }}>
+                    <Popup>Popup in FeatureGroup</Popup>
+                    <Circle center={[0, 0]} radius={200} />
+                    <Rectangle bounds={rectangle} />
+                </FeatureGroup>
+            </LayersControl.Overlay> */}
         </>
     )
 }
--- src/components/MapControls/index.tsx
+++ src/components/MapControls/index.tsx
@@ -1,19 +1,10 @@
-import { LatLngBoundsExpression, LatLngExpression } from 'leaflet'
 import { useContext } from 'react';
-import { Circle, FeatureGroup, LayerGroup, LayersControl, Marker, Popup, Rectangle } from 'react-leaflet'
+import { LayersControl } from 'react-leaflet'
 import { IntelContext } from '../../contexts/IntelContext/IntelContextProvider';
-import { MapItem } from '../../helpers/models';
-import { DefaultIcon } from '../DefaultIcon';
-import { MapControlsProps, MapMenuItem } from './types';
+import { IntelMarkers } from '../IntelMarkers';
 
 export const MapControls = () => {
-    const center: LatLngExpression = [0, 0]
-    const rectangle: LatLngBoundsExpression = [
-        [0, 0],
-        [0, 0],
-    ]
-    const { intelArtifactMarkers, intelAudioMarkers, currentMap, currentMapGroup } = useContext(IntelContext)
-
+    const { currentMap, currentMapGroup } = useContext(IntelContext)
     return (
         <LayersControl position="topright">
             {currentMapGroup.mapLayers.map(mapLayer =>
@@ -21,43 +12,7 @@ export const MapControls = () => {
                     {mapLayer.mapOverlay}
                 </LayersControl.BaseLayer>
             )}
-            <LayersControl.Overlay checked name="Intel- Artifact Markers">
-                <LayerGroup>
-                    {intelArtifactMarkers}
-                </LayerGroup>
-            </LayersControl.Overlay>
-            <LayersControl.Overlay checked name="Intel- Artifact Markers">
-                {intelAudioMarkers}
-            </LayersControl.Overlay>
-            <LayersControl.Overlay checked name="Misc Markers">
-                <LayerGroup>
-                    <Circle
-                        center={center}
-                        pathOptions={{ fillColor: 'blue' }}
-                        radius={200}
-                    />
-                    <Circle
-                        center={center}
-                        pathOptions={{ fillColor: 'red' }}
-                        radius={100}
-                        stroke={false}
-                    />
-                    <LayerGroup>
-                        <Circle
-                            center={[0, 0]}
-                            pathOptions={{ color: 'green', fillColor: 'green' }}
-                            radius={100}
-                        />
-                    </LayerGroup>
-                </LayerGroup>
-            </LayersControl.Overlay>
-            <LayersControl.Overlay name="Easter Eggs">
-                <FeatureGroup pathOptions={{ color: 'purple' }}>
-                    <Popup>Popup in FeatureGroup</Popup>
-                    <Circle center={[0, 0]} radius={200} />
-                    <Rectangle bounds={rectangle} />
-                </FeatureGroup>
-            </LayersControl.Overlay>
+            <IntelMarkers />
         </LayersControl>
     )
 }
