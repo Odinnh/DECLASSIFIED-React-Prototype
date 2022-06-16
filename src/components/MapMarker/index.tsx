ece711fc6154ab6feafed1b88e61e254a8d808f6
Ben A <ben@sol3.me>
2022-06-17 00:28:33 +0100

    Fix map markers with proper icons

--- src/components/MapMarker/index.tsx
+++ src/components/MapMarker/index.tsx
@@ -1,19 +1,22 @@
+import L, { DivIconOptions, PointExpression } from 'leaflet'
 import React from 'react'
 import { Marker, Popup } from 'react-leaflet'
 import { IntelMarker, MapItem } from '../../helpers/models'
 import { DefaultIcon } from '../DefaultIcon'
 
-export const IntelMapMarker = ({ id, title, desc, icon, layer, typeDesc, loc, faction, season, img }: IntelMarker) => {
-    let imgSrc = img ? `https://i.imgur.com/${img}.jpg` :'assets/img/intelScreenshot/placeholder.png';
+export const IntelMapMarker = ({ id, title, desc, typeDesc, loc, faction, season, img }: IntelMarker) => {
+    let imgSrc = img ? `https://i.imgur.com/${img}.jpg` : 'assets/img/intelScreenshot/placeholder.png';
+    // map
+    const markerIcon = intelIconInit(faction, typeDesc);
 
     return (
-        <Marker key={id} position={loc} icon={DefaultIcon}>
+        <Marker key={id} position={loc} icon={markerIcon}>
             <Popup>
                 <>
                     <h1>{title}</h1>
 
                     <div className="intel-content">
-                    <div>
+                        <div>
                             <p>{desc}</p>
                             <div className="buttonContainer" data-item={id}>
                                 {/* {collectedBtn}
@@ -28,4 +31,26 @@ export const IntelMapMarker = ({ id, title, desc, icon, layer, typeDesc, loc, fa
             </Popup>
         </Marker>
     )
+}
+
+function intelIconInit(faction, type) {
+    const iconHtml =
+        `<div>
+        <img class="icon" src="assets/img/icons/type/${type}.png" alt="Icon">
+        <img class="background" src="assets/img/icons/markers/${faction}.png" alt="Background">
+        <!-- Shadow here -->
+    </div>`;
+
+    var markerIcons: DivIconOptions = {
+        html: iconHtml,
+        className: 'intel-icon',
+        iconSize: [25, 25],
+        iconAnchor: [12.5, 40],
+        shadowSize: [33, 44],
+        shadowAnchor: [(33 / 2), 44],
+        popupAnchor: [0, -25],
+    }
+    return L.divIcon(
+        markerIcons
+    );
 }
