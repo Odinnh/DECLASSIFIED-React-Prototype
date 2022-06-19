f39dda8d5fd797d44af5d40951f16711c3cbdec3
Ben A <ben@sol3.me>
2022-06-19 01:57:28 +0100

    Add SwipeableDrawer for intel menu

---
+++ src/components/IntelList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IntelList = () => {
+    return (
+        <div>
+            <button>
+                intelItem name
+            </button>
+        </div>
+    )
+}
--- src/components/IntelMenu/index.tsx
+++ src/components/IntelMenu/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { SwipeableDrawer } from '@mui/material'
+import React, { useContext } from 'react'
+import { IntelContext } from '../../contexts/IntelContext/IntelContextProvider';
+import { IntelList } from '../IntelList'
+
+
 
 export const IntelMenu = () => {
+    const { drawerState, toggleDrawer} = useContext(IntelContext);
+
     return (
-        <div>IntelMenu</div>
+        <SwipeableDrawer
+            anchor={'bottom'}
+            open={drawerState['bottom']}
+            onClose={toggleDrawer('bottom', false)}
+            onOpen={toggleDrawer('bottom', true)}
+        >
+            <div>IntelMenu</div>
+            <IntelList />
+            {/* <ExpandableMenu>
+            <IntelFilter />
+            <ListButtons />
+        </ExpandableMenu> */}
+        </SwipeableDrawer>
     )
 }
