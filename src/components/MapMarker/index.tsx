8cade55f9d6092d37251f798fe83af5344507036
Ben A <ben@sol3.me>
2022-08-03 00:12:41 +0100

    require icon images properly

--- src/components/MapMarker/index.tsx
+++ src/components/MapMarker/index.tsx
@@ -1,8 +1,19 @@
-import L, { DivIconOptions, PointExpression } from 'leaflet'
-import React from 'react'
+import L, { DivIconOptions } from 'leaflet'
 import { Marker, Popup } from 'react-leaflet'
-import { IntelMarker, MapItem } from '../../helpers/models'
-import { DefaultIcon } from '../DefaultIcon'
+import { IntelMarker } from '../../helpers/models'
+import ReactDOMServer from 'react-dom/server';
+import { Faction, IntelType } from '../../data/intel';
+
+const iconImages = {
+    [IntelType.Artifact]: require('../../assets/img/icons/type/artifacts.png'),
+    [IntelType.Audio]: require('../../assets/img/icons/type/audio logs.png')
+}
+const iconBackgrounds = {
+    [Faction.DarkAether]: require('../../assets/img/icons/markers/dark aether.png'),
+    [Faction.Maxis]: require('../../assets/img/icons/markers/maxis.png'),
+    [Faction.Omega]: require('../../assets/img/icons/markers/omega.png'),
+    [Faction.Requiem]: require('../../assets/img/icons/markers/requiem.png')
+}
 
 export const IntelMapMarker = ({ id, title, desc, typeDesc, loc, faction, season, img }: IntelMarker) => {
     let imgSrc = img ? `https://i.imgur.com/${img}.jpg` : 'assets/img/intelScreenshot/placeholder.png';
@@ -25,7 +36,7 @@ export const IntelMapMarker = ({ id, title, desc, typeDesc, loc, faction, season
                                 {moreBtn} */}
                             </div>
                         </div>
-                        <img src={imgSrc} /* onClick="expandImage(this)" */></img>
+                        <img src={imgSrc} /* onClick="expandImage(this)" */ alt="Intel Location"></img>
                     </div>
                 </>
             </Popup>
@@ -33,16 +44,18 @@ export const IntelMapMarker = ({ id, title, desc, typeDesc, loc, faction, season
     )
 }
 
-function intelIconInit(faction, type) {
-    const iconHtml =
-        `<div>
-        <img class="icon" src="assets/img/icons/type/${type}.png" alt="Icon">
-        <img class="background" src="assets/img/icons/markers/${faction}.png" alt="Background">
-        <!-- Shadow here -->
-    </div>`;
+const intelIconInit = (faction: Faction, type: string) => {
+    const iconImage = iconImages[type];
+    const iconBackground = iconBackgrounds[faction];
 
     var markerIcons: DivIconOptions = {
-        html: iconHtml,
+        html: ReactDOMServer.renderToString(
+            <div>
+                <img className='icon' src={iconImage} alt="Icon" />
+                <img className='background' src={iconBackground} alt="Background" />
+            </div>
+
+        ),
         className: 'intel-icon',
         iconSize: [25, 25],
         iconAnchor: [12.5, 40],
@@ -50,7 +63,8 @@ function intelIconInit(faction, type) {
         shadowAnchor: [(33 / 2), 44],
         popupAnchor: [0, -25],
     }
+
     return L.divIcon(
         markerIcons
     );
-}
+};
