cf20ac3dab1bd10374246336a544538964576d3d
Ben A <ben@sol3.me>
2022-08-04 17:31:19 +0100

    Class improvements to avoid recursive type references

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,6 +1,7 @@
+import { generalIcon } from './../components/MiscMapMarker/index';
 import { Icon, IconOptions, LatLngExpression } from "leaflet";
-import { Faction, Season } from "../data/intel";
-import { generalIcon, renderIntelMapMarkers, renderMiscMapMarkers } from "../helpers/markers";
+import { Faction, IconTypes, Season } from "../data/intel";
+// import { generalIcon, renderIntelMapMarkers, renderMiscMapMarkers } from "../helpers/markers";
 
 /////////////////////Classes/////////////////////////
 
@@ -8,21 +9,21 @@ export interface IItem {
     id?: string;
     title: string;
     desc?: string;
-    icon?: Icon<IconOptions>;
+    icon?: string;
     layer?: string;
 }
 export class Item {
     id?: string;
     title: string;
     desc?: string;
-    icon?: Icon<IconOptions>;
+    icon?: string;
     layer?: string;
 
     constructor({ id, title, desc, icon, layer }: IItem) {
         this.id = id;
         this.title = title ?? "";
         this.desc = desc ?? "";
-        this.icon = icon ?? generalIcon;
+        this.icon = icon ?? IconTypes.generalIcon;
         this.layer = layer ?? "MiscMarkers";
     }
 }
@@ -31,7 +32,7 @@ export class BaseMarker extends Item {
     typeDesc: string;
     loc: LatLngExpression;
 
-    constructor({ id, title, desc, icon, layer, typeDesc, loc }: BaseMarker) {
+    constructor({ id, title, desc, icon, layer, typeDesc, loc }: any) {
         super({ id, title, desc, icon, layer });
         this.typeDesc = typeDesc;
         this.loc = loc ?? [0, 0];
@@ -43,7 +44,7 @@ export class IntelMarker extends BaseMarker {
     season: Season;
     img?: string;
 
-    constructor({ id, title, desc, icon, layer, typeDesc, loc, faction, season, img }: IntelMarker) {
+    constructor({ id, title, desc, icon, layer, typeDesc, loc, faction, season, img }: any) {
         super({ id, title, desc, icon, layer, typeDesc, loc });
         this.faction = faction;
         this.season = season;
@@ -52,14 +53,14 @@ export class IntelMarker extends BaseMarker {
 }
 
 export class MiscMarker extends BaseMarker {
-    constructor(id: string, { title, desc, icon }: Item, loc: LatLngExpression, uniqueDesc?: string) {
+    constructor(id: string, { title, desc, icon }: any, loc: LatLngExpression, uniqueDesc?: string) {
         desc = uniqueDesc ?? desc
         super({ id, title, desc, icon, loc, typeDesc: "Misc" })
     }
 }
 
 export class MiscType extends Item {
-    constructor({ id, title, desc, icon, layer }: Item) {
+    constructor({ id, title, desc, icon, layer }: any) {
         super({ id, title, desc, icon, layer });
     }
 }
@@ -68,10 +69,8 @@ export class MapItem extends Item {
     mapOverlay?: JSX.Element;
     intelMapMarkers?: JSX.Element[];
     miscMapMarkers?: JSX.Element[];
-    constructor(id, { title, desc, icon, layer, mapOverlay }: MapItem) {
+    constructor(id, { title, desc, icon, layer, mapOverlay }: any) {
         super({ id, title, desc, icon, layer });
         this.mapOverlay = mapOverlay;
-        // this.intelMapMarkers = renderIntelMapMarkers(id);
-        // this.miscMapMarkers = renderMiscMapMarkers(id);
     }
 }
