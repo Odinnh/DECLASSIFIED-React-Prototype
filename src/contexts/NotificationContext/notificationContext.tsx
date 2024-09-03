5c32d040649fa376042c781b0fef3411ad747ee5
sol3uk <ben@sol3.me>
2024-09-03 22:56:57 +0100

    Tidy

--- src/contexts/NotificationContext/notificationContext.tsx
+++ src/contexts/NotificationContext/notificationContext.tsx
@@ -1,14 +1,11 @@
 import { createContext, useContext } from 'react';
 
-// Define the shape of your context
 interface NotificationContextProps {
     triggerNotification: (message: string) => void;
 }
 
-// Create the context
 export const NotificationContext = createContext<NotificationContextProps | undefined>(undefined);
 
-// Custom hook to use the context
 export const useNotification = () => {
     const context = useContext(NotificationContext);
     if (!context) {
--- src/contexts/NotificationContext/notificationProvider.tsx
+++ src/contexts/NotificationContext/notificationProvider.tsx
@@ -1,13 +1,10 @@
 import React, { ReactNode, useRef } from 'react';
-import { Snackbar } from '@mui/material';
 import { NotificationContext } from './notificationContext';
 import NotificationBanner from '../../components/NotificationBanner/NotificationBanner';
 
-// The NotificationProvider component
 export const NotificationProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const snackbarRef = useRef<{ handleClick: (msg: string) => void }>(null);
 
-    // The notification function
     const triggerNotification = (notificationMessage: string) => {
         if (snackbarRef.current) {
             snackbarRef.current.handleClick(notificationMessage);
@@ -17,8 +14,6 @@ export const NotificationProvider: React.FC<{ children: ReactNode }> = ({ childr
     return (
         <NotificationContext.Provider value={{ triggerNotification }}>
             {children}
-
-            {/* Snackbar component where the ref will be used */}
             <NotificationBanner ref={snackbarRef} />
         </NotificationContext.Provider>
     );
